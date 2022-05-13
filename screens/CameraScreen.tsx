f33505042e332cef388e6bbc989c3ee45c8e002e
Jeffrey Zhou <jeffreyzh@icloud.com>
2022-05-13 13:25:31 -0700

    added camera functionality

--- screens/CameraScreen.tsx
+++ screens/CameraScreen.tsx
@@ -1,13 +1,108 @@
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  _Text,
+  ImageBackground,
+} from "react-native";
+import { Camera } from "expo-camera";
 
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+export default function App() {
+  let camera: Camera;
 
-export default function CameraScreen({ navigation }: RootTabScreenProps<'Camera'>) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState<any>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  const __takePicture = async () => {
+    console.log("qwekgjqwlgjass");
+    console.log(camera);
+
+    if (!camera) return;
+    const photo = await camera.takePictureAsync();
+    console.log("awegjkask");
+    console.log(photo);
+    setPreviewVisible(true);
+    setCapturedImage(photo);
+  };
+  const CameraPreview = ({ photo }: any) => {
+    console.log("sdsfds", photo);
+    return (
+      <View
+        style={{
+          backgroundColor: "transparent",
+          flex: 1,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <ImageBackground
+          source={{ uri: photo && photo.uri }}
+          style={{
+            flex: 1,
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Camera Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        ref={(r) => {
+          camera = r;
+        }}
+      >
+        <View
+          style={{
+            position: "absolute",
+            bottom: 0,
+            flexDirection: "row",
+            flex: 1,
+            width: "100%",
+            padding: 20,
+            justifyContent: "space-between",
+          }}
+        >
+          <View
+            style={{
+              alignSelf: "center",
+              flex: 1,
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity
+              onPress={__takePicture}
+              style={{
+                width: 70,
+                height: 70,
+                bottom: 0,
+                borderRadius: 50,
+                backgroundColor: "#fff",
+              }}
+            />
+          </View>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -15,16 +110,23 @@ export default function CameraScreen({ navigation }: RootTabScreenProps<'Camera'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  camera: {
+    flex: 1,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
   },
 });
