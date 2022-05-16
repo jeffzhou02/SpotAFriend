0674ab345ac50c9c41941ea8145f2716405d70e0
nathanzzhang <70558078+nathanzzhang@users.noreply.github.com>
2022-05-15 23:43:35 -0700

    not working cloud database

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize'); // import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('loginDB', 'root', 'CS35LSpotafriend', {
+const sequelize = new Sequelize('loginDB', 'root', 'password', {
     dialect: 'mysql',
-    host: 'localhost', 
+    host: '/cloudsql/spotafriend:us-west2:spotafriend',
+    dialectOptions: {
+        socketPath: '/cloudsql/spotafriend:us-west2:spotafriend'
+    } 
 });
 
 module.exports = sequelize;
