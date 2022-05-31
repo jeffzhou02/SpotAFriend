84a929baa22191dd1331cb5aa713a8884cfa7645
Nathan Zhang <70558078+nathanzzhang@users.noreply.github.com>
2022-05-30 23:46:22 -0700

    Nathan/profile upload (#51)
    * updated README
    
    * updated packages to include ui kitten
    
    * rough working navbar
    
    * updated packages
    
    * improved navbar icons
    
    * preliminary files
    
    * added signup and login button functionality
    
    * working backend
    
    * reorganized backend
    
    * updated packages
    
    * not working cloud database
    
    * local backend
    
    * cloud db
    
    * added yaml
    
    * not working cloud
    
    * updated cloud
    
    * updates
    
    * not working
    
    * added private route
    
    * changed socketpath
    
    * some changes
    
    * works on web, not mobile
    
    * final backend commit
    
    * firebase started
    
    * trying
    
    * working!!!:
    
    * finished login minus useContext
    
    * removed console logs
    
    * finished signup functionality
    
    * setup global user context
    
    * finished user context
    
    * fixed merge conflict
    
    * fixed merge conflict
    
    * fixed merge conflict
    
    * fixed signups
    
    * fixed signup
    
    * fixed packages
    
    * updated styles
    
    * somewhat working photo upload
    
    * preparing to downgrade
    
    * preparing to downgrade
    
    * updated packages
    
    Co-authored-by: Nathan Zhang <nathanzzhang@ucla.edu>

--- frontend/screens/ProfileScreen.tsx
+++ frontend/screens/ProfileScreen.tsx
@@ -1,30 +1,49 @@
-import { StyleSheet, TouchableOpacity, Image } from "react-native";
-import { useContext } from "react";
-import { Text, View } from "../components/Themed";
-import { RootStackParamList } from "../types";
-import { UserContext } from "../components/UserContext";
+import { StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { useContext, useState } from 'react';
+import { Text, View } from '../components/Themed';
+import { RootStackParamList } from '../types';
+import { UserContext } from '../components/UserContext';
+import { storage } from '../firebase/index';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function ProfileScreen({
-  navigation,
-}: RootStackParamList<"Root">) {
-  const name = "asdfasdf";
+//remeber to add profile photo later
+export default function ProfileScreen({ navigation }: RootStackParamList<'Root'>) {
+  const storageRef = ref(storage, 'profilephotos');
+  const [image, setImage] = useState("");
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setImage(result.uri);
+      uploadImageAsync(result.uri);
+    }
+  };
+  const name = 'asdfasdf';
   const { user } = useContext(UserContext);
   const cancelFunction = () => navigation.navigate("Profile");
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.pfp}
-        source={{
-          uri: "https://i.scdn.co/image/ab67616d00001e021cf64730713292322465d339",
-        }}
-      />
-      <InfoView
-        name={name}
-        username={user.username}
-        email={user.email}
-        usernameHandler={() =>
-          navigation.navigate("EditInfo", {
-            name: "username",
+      {
+        image === "" ? null :
+          <View>
+            <Image style={styles.targetImage} source={{ uri: image }} />
+          </View>
+      }
+      <TouchableOpacity style={styles.row} onPress={pickImage}>
+        <View style={styles.label}>
+          <Text>edit photo</Text>
+        </View>
+      </TouchableOpacity>
+      <InfoView name={name} username={user.username} email={user.email}
+        usernameHandler={() => navigation.navigate(
+          'EditInfo',
+          {
+            name: 'username',
             cancel: cancelFunction,
             attrib: "username",
             initial: user.username,
@@ -40,37 +59,57 @@ export default function ProfileScreen({
         }
       />
       <View>
-        <View style={styles.box2}>
-          <TouchableOpacity>
-            <Text style={styles.textStyle}>change profile picture</Text>
-          </TouchableOpacity>
-        </View>
-        <Text></Text>
+        <Text>
+
+        </Text>
       </View>
     </View>
   );
 }
 
+async function uploadImageAsync(uri: any) {
+  console.log("uploading...");
+  // uri = uri.replace("file:///", "file:/");
+  console.log(uri);
+
+  // const blob = await new Promise((resolve, reject) => {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.onload = function () {
+  //     resolve(xhr.response);
+  //   };
+  //   xhr.onerror = function (e) {
+  //     console.log(e);
+  //     reject(new TypeError("Network request failed"));
+  //   };
+  //   xhr.responseType = "blob";
+  //   xhr.open("GET", uri, true);
+  //   xhr.send();
+  // });
+  console.log("done making blob");
+  const fileRef = ref(storage, 'profilephotos/user.jpg');
+  console.log("done making fileRef");
+  const img = await fetch(uri);
+  console.log("done fetching");
+  const bytes = await img.blob();
+  console.log("done bytes");
+  const result = await uploadBytes(fileRef, bytes);
+  console.log("uploaded!");
+
+  // We're done with the blob, close and release it
+  // blob.close();
+
+  return await getDownloadURL(fileRef);
+}
+
 function InfoView(props: any) {
   return (
-    <View
-      style={{
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#E3DAC9",
-      }}
-    >
-      <RowButton
-        label="username:"
-        data={props.username}
-        onPress={props.usernameHandler}
-      />
-      <RowButton
-        label="email:"
-        data={props.email}
-        onPress={props.emailHandler}
-        style={{ marginTop: 10 }}
-      />
+    <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: "#E3DAC9", }}>
+
+      <Divider />
+      <RowButton label='username' data={props.username} onPress={props.usernameHandler} />
+      <Divider />
+      <RowButton label='email' data={props.email} onPress={props.emailHandler} />
+      <Divider />
     </View>
   );
 }
@@ -120,23 +159,25 @@ function Divider() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center", //can change to flex-start if need to push to top
+    alignItems: 'center',
+    justifyContent: 'center', //can change to flex-start if need to push to top
     backgroundColor: "#E3DAC9",
   },
   label: {
     width: "30%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: "#E3DAC9",
+
   },
   data: {
     width: "70%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: "#E3DAC9",
+
   },
   row: {
     width: "90%",
@@ -145,54 +186,32 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 15,
     backgroundColor: "#E3DAC9",
-    borderColor: "#689689",
-    borderWidth: 2,
-    borderRadius: 20,
-    margin: 10,
-  },
-  box2: {
-    backgroundColor: "#F4D58D",
-    borderColor: "#689689",
-    borderWidth: 2,
-    borderRadius: 17,
-    marginTop: "5%",
-    marginBottom: "5%",
-    paddingHorizontal: "2%",
-    paddingVertical: "2%",
-    marginHorizontal: "2%",
-    alignItems: "center",
-    width: 350,
   },
   divider: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: "#E3DAC9",
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
+
   },
   separator: {
     marginVertical: 1,
     height: 1,
-    width: "85%",
-    backgroundColor: "rgba(0,0,0,0.2)",
+    width: '85%',
+    backgroundColor: "#E3DAC9",
+
   },
-  pfp: {
+  targetImage: {
     alignSelf: "center",
-    height: 180,
-    width: 180,
-    borderRadius: 90,
-    borderWidth: 3,
-    borderColor: "#689689",
-    marginBottom: "5%",
-  },
-  textStyle: {
-    fontSize: 18,
-    fontStyle: "italic",
-    fontWeight: "bold",
-    color: "#689689",
-    backgroundColor: "transparent",
-  },
+    height: 140,
+    width: 140,
+    borderRadius: 70,
+    borderWidth: 2,
+    borderColor: "#FFF",
+    margin: "5%",
+  }
 });
