136fc21cf1b5c7db8cc8bfc08105dfb8f1031786
Jenny Wang <52713614+jennywnuo@users.noreply.github.com>
2022-05-30 22:32:00 -0700

    prettier, lol poggers (#50)

--- frontend/screens/ProfileScreen.tsx
+++ frontend/screens/ProfileScreen.tsx
@@ -1,54 +1,76 @@
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import { useContext } from 'react';
-import { Text, View } from '../components/Themed';
-import { RootStackParamList } from '../types';
-import { UserContext } from '../components/UserContext';
+import { StyleSheet, TouchableOpacity, Image } from "react-native";
+import { useContext } from "react";
+import { Text, View } from "../components/Themed";
+import { RootStackParamList } from "../types";
+import { UserContext } from "../components/UserContext";
 
-//remeber to add profile photo later
-export default function ProfileScreen({ navigation }: RootStackParamList<'Root'>) {
-  const name='asdfasdf';
+export default function ProfileScreen({
+  navigation,
+}: RootStackParamList<"Root">) {
+  const name = "asdfasdf";
   const { user } = useContext(UserContext);
   const cancelFunction = () => navigation.navigate("Profile");
   return (
     <View style={styles.container}>
-      <InfoView name={name} username={user.username} email={user.email}
-        usernameHandler={() => navigation.navigate(
-          'EditInfo',
-          {
-            name: 'username',
+      <Image
+        style={styles.pfp}
+        source={{
+          uri: "https://i.scdn.co/image/ab67616d00001e021cf64730713292322465d339",
+        }}
+      />
+      <InfoView
+        name={name}
+        username={user.username}
+        email={user.email}
+        usernameHandler={() =>
+          navigation.navigate("EditInfo", {
+            name: "username",
             cancel: cancelFunction,
-            attrib: 'username',
+            attrib: "username",
             initial: user.username,
           })
         }
-        emailHandler={() => navigation.navigate(
-          'EditInfo',
-          {
-            name: 'email',
+        emailHandler={() =>
+          navigation.navigate("EditInfo", {
+            name: "email",
             cancel: cancelFunction,
-            attrib: 'email',
+            attrib: "email",
             initial: user.email,
           })
         }
       />
       <View>
-        <Text>
-
-        </Text>
+        <View style={styles.box2}>
+          <TouchableOpacity>
+            <Text style={styles.textStyle}>change profile picture</Text>
+          </TouchableOpacity>
+        </View>
+        <Text></Text>
       </View>
     </View>
   );
 }
 
 function InfoView(props: any) {
-  return(
-    <View style={{alignItems: 'center', justifyContent: 'center', backgroundColor: "#E3DAC9",  }}>
-
-      <Divider/>
-      <RowButton label='username' data={props.username} onPress={props.usernameHandler}/>
-      <Divider/>
-      <RowButton label='email' data={props.email} onPress={props.emailHandler}/>
-      <Divider/>
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#E3DAC9",
+      }}
+    >
+      <RowButton
+        label="username:"
+        data={props.username}
+        onPress={props.usernameHandler}
+      />
+      <RowButton
+        label="email:"
+        data={props.email}
+        onPress={props.emailHandler}
+        style={{ marginTop: 10 }}
+      />
     </View>
   );
 }
@@ -57,10 +79,27 @@ function RowButton(props: any) {
   return (
     <TouchableOpacity style={styles.row} onPress={props.onPress}>
       <View style={styles.label}>
-        <Text>{props.label}</Text>
+        <Text
+          style={{
+            color: "#689689",
+            fontSize: 17,
+            fontStyle: "italic",
+            fontWeight: "bold",
+          }}
+        >
+          {props.label}
+        </Text>
       </View>
       <View style={styles.data}>
-        <Text>{props.data}</Text>
+        <Text
+          style={{
+            color: "#689689",
+            fontSize: 17,
+            fontStyle: "italic",
+          }}
+        >
+          {props.data}
+        </Text>
       </View>
     </TouchableOpacity>
   );
@@ -69,7 +108,11 @@ function RowButton(props: any) {
 function Divider() {
   return (
     <View style={styles.divider}>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
     </View>
   );
 }
@@ -77,50 +120,79 @@ function Divider() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center', //can change to flex-start if need to push to top
+    alignItems: "center",
+    justifyContent: "center", //can change to flex-start if need to push to top
     backgroundColor: "#E3DAC9",
   },
   label: {
     width: "30%",
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
     backgroundColor: "#E3DAC9",
-
   },
   data: {
     width: "70%",
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
     backgroundColor: "#E3DAC9",
-
   },
   row: {
-    width: '90%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "90%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 15,
     backgroundColor: "#E3DAC9",
+    borderColor: "#689689",
+    borderWidth: 2,
+    borderRadius: 20,
+    margin: 10,
+  },
+  box2: {
+    backgroundColor: "#F4D58D",
+    borderColor: "#689689",
+    borderWidth: 2,
+    borderRadius: 17,
+    marginTop: "5%",
+    marginBottom: "5%",
+    paddingHorizontal: "2%",
+    paddingVertical: "2%",
+    marginHorizontal: "2%",
+    alignItems: "center",
+    width: 350,
   },
   divider: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#E3DAC9",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 1,
     height: 1,
-    width: '85%',
+    width: "85%",
     backgroundColor: "rgba(0,0,0,0.2)",
-
+  },
+  pfp: {
+    alignSelf: "center",
+    height: 180,
+    width: 180,
+    borderRadius: 90,
+    borderWidth: 3,
+    borderColor: "#689689",
+    marginBottom: "5%",
+  },
+  textStyle: {
+    fontSize: 18,
+    fontStyle: "italic",
+    fontWeight: "bold",
+    color: "#689689",
+    backgroundColor: "transparent",
   },
 });
